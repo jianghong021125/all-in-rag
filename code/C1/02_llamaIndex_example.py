fa575ad262ed145bdff705ec0e9008eafc8cf5f4
jianghong <jianghong021125@gmail.com>
2026-05-19 15:25:29 +0800

    Chapter 1 homework done; Develop 5 chunking parameter test sets and generate content only + cache hit rate performance; comment for llamaindex file.

--- code/C1/02_llamaIndex_example.py
+++ code/C1/02_llamaIndex_example.py
@@ -2,32 +2,38 @@ import os
 # os.environ['HF_ENDPOINT']='https://hf-mirror.com'
 from dotenv import load_dotenv
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings 
-from llama_index.llms.openai_like import OpenAILike
+from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 load_dotenv()
 
 # 使用 AIHubmix
-Settings.llm = OpenAILike(
-    model="glm-4.7-flash-free",
-    api_key=os.getenv("DEEPSEEK_API_KEY"),
-    api_base="https://aihubmix.com/v1",
-    is_chat_model=True
-)
-
-# Settings.llm = OpenAI(
-#     model="deepseek-chat",
+# Settings.llm = OpenAILike(
+#     model="glm-4.7-flash-free",
 #     api_key=os.getenv("DEEPSEEK_API_KEY"),
-#     api_base="https://api.deepseek.com"
+#     api_base="https://aihubmix.com/v1",
+#     is_chat_model=True
 # )
+# 使用 DeepSeek
+Settings.llm = OpenAI(
+    model="deepseek-chat",
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    api_base="https://api.deepseek.com"
+)
+# 中文 Embedding 模型
 Settings.embed_model = HuggingFaceEmbedding("BAAI/bge-small-zh-v1.5")
 
+# 加载本地markdown文件
 docs = SimpleDirectoryReader(input_files=["../../data/C1/markdown/easy-rl-chapter1.md"]).load_data()
 
+# 构建向量索引
 index = VectorStoreIndex.from_documents(docs)
 
+# 创建查询引擎
 query_engine = index.as_query_engine()
 
+# 提示词模板使用
 print(query_engine.get_prompts())
 
+# 查询问题
 print(query_engine.query("文中举了哪些例子?"))
